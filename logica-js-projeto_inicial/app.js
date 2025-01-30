193c0d5793462f261a71751062c97fda6f3bed49
Matteo <146030856+Matteoziinn@users.noreply.github.com>
2025-01-30 16:50:02 -0300

    app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,32 +1,41 @@
+// Exibe uma mensagem de boas-vindas ao usuário
 alert('Boas vindas ao jogo do número secreto');
+
+// Define o valor máximo para o número secreto
 let numeroMaximo = 100;
+
+// Gera um número secreto aleatório entre 1 e numeroMaximo
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+
+// Exibe o número secreto no console (para fins de debug)
 console.log(numeroSecreto);
+
 let chute;
 let tentativas = 1;
 
-// enquanto chute não for igual ao n.s.
+// Enquanto o chute não for igual ao número secreto
 while (chute != numeroSecreto) {
+    // Solicita ao usuário que escolha um número
     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-    // se chute for igual ao número secreto
+    
+    // Se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        break;
+        break; // Sai do loop
     } else {
+        // Se o chute for maior que o número secreto
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
+            // Se o chute for menor que o número secreto
             alert(`O número secreto é maior que ${chute}`);
         }
-        // tentativas = tentativas + 1;
+        // Incrementa o número de tentativas
         tentativas++;
     }
 }
 
+// Define a palavra correta para "tentativa" no plural ou singular
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
-// if (tentativas > 1) {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-// } else {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-// }
+// Informa ao usuário que ele acertou o número secreto e quantas tentativas foram necessárias
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
